15ff16dfe74eeeb94c2dbc7a64514bb2e5baa5eb
besthero3 <57282024+besthero3@users.noreply.github.com>
2025-06-10 09:49:05 -0700

    Started result processing after checking if the notation is valid and seeing which piece is being moved. Created a function that converts notation to the square a piece should be moved to. Decided that for checking if a move is valid, iteration will start at the square that the piece is trying to be moved to.

--- src/MainCLI.java
+++ src/MainCLI.java
@@ -15,13 +15,13 @@ public class MainCLI {
 
     public static final String BLACK = "\u001B[30m";
     public static final String WHITE = "\u001B[37m";
+    public static final String GREEN = "\u001B[32m";
 
 
 
     //board has square
     //TODO: could set 0,0 to be a1
     static Square[][] board = new Square[8][8];
-
 
     public static void main(String[] args) {
 
@@ -34,9 +34,145 @@ public class MainCLI {
         }
         createBoard();
 
+        while(!gameOver) {
+            printBoard();
+
+            Scanner input = new Scanner(System.in);
+
+            //white's move
+            if(whoseMove) {
+                System.out.println("White, what is your move?");
+            }
+            else {
+                System.out.println("Black, what is your move?");
+            }
+
+            //gets the move
+            String move = input.nextLine();
+
+            String notationCheck = checkNotation(move);
+
+            if (!notationCheck.equals("Invalid")) {
+                boolean validMove = false;
+                boolean captures = false;
+                int[] coordinates = new int[2];
+
+                if (notationCheck.contains("takes")) {
+                    captures = true;
+                }
+
+                //TODO: find square that the piece is on depending on which piece.. we have
+                //iterate through it and look for a piece with that color... see if we have iterations
+                //TODO could start at square that we are looking at and iterate in necessary directions...
+                //TODO: get square that we are going to for string parsing
+
+                //contains because takes can be present...
+                if (notationCheck.contains("Rook")) {
+
+                    //Rf4
+                    if (move.length() == 3) {
+                        coordinates = convertMoveToSquare(move.charAt(1), (int) move.charAt(2));
+                    }
+
+                    //Rxf4
+                    if (move.length() == 4 && captures) {
+                        coordinates = convertMoveToSquare(move.charAt(2), (int) move.charAt(3));
+                    }
+
+                    //TODO: Rfe4
+                    //TODO: Rfxe4
+
+                    //TODO: 4 length without captures...then Rfe8 make sure to process the second number...
+                    //captures - Rxf4, check that there is a piece on f4 of the opposite color, then look for rook
+                    //not captures Rf4, check surrounding f4 for a rook of the correct color
+
+                    if (whoseMove) {
+                        Rook rook = new Rook(PieceColor.WHITE);
+                        validMove = rook.isValid(0,0); //check if the format is right
+                    }
+                    else {
+                        Rook rook = new Rook(PieceColor.BLACK);
+                        validMove = rook.isValid(0,0); //check if the format is right
+                    }
+
+                    if (validMove) {
+
+                    }
+                    else {
+
+                    }
+
+
+                }
+                if (notationCheck.contains("Knight")) {
+
+                }
+                if (notationCheck.contains("Bishop")) {
+
+                }
+                if (notationCheck.contains("Queen")) {
+
+                }
+                if (notationCheck.contains("King")) {
+
+                }
+
+                //call different class to check move validation
+                if (inCheck) {
+                    //ensure that the check is working
+                }
+
+                //if valid move
+                //check if last move put king in check
+
+                whoseMove = !whoseMove;
+            }
+        }
+    }
+
+    static int[] convertMoveToSquare(char col, int row) {
+        int[] return_array = new int[2];
+
+        return_array[0] = (row - 8) * -1;
+
+        if(col == 'a') {
+            return_array[1] = 0;
+        }
+        else if(col == 'b') {
+            return_array[1] = 1;
+        }
+        else if(col == 'c') {
+            return_array[1] = 2;
+        }
+        else if(col == 'd') {
+            return_array[1] = 3;
+        }
+        else if(col == 'e') {
+            return_array[1] = 4;
+        }
+        else if(col == 'f') {
+            return_array[1] = 5;
+        }
+        else if(col == 'g') {
+            return_array[1] = 6;
+        }
+        else if(col == 'h') {
+            return_array[1] = 7;
+        }
+
+        return return_array;
+    }
+
+    static void printBoard() {
         //prints the board
         //TODO: can play with background
+        System.out.println(GREEN + "    a b c d e f g h " + WHITE);
+        System.out.println(GREEN + "    ________________" + WHITE);
+
+        int borderCounter = 8;
         for(int row = 0; row < board[0].length; row++) {
+
+            System.out.print(GREEN + (borderCounter - row) + " | " + WHITE);
 
             for(int col = 0; col < board.length; col++) {
 
@@ -123,37 +259,17 @@ public class MainCLI {
 
                 }
             }
+            //System.out.print(GREEN +  " | " + (borderCounter - row) + WHITE);
             System.out.println();
 
         }
+        //end of it
+        System.out.println(GREEN + "    ________________" + WHITE);
+        System.out.println(GREEN + "    a b c d e f g h " + WHITE);
 
-        while(!gameOver) {
-            Scanner input = new Scanner(System.in);
-
-            //white's move
-            if(whoseMove) {
-                System.out.println("White, what is your move?");
-            }
-            else {
-                System.out.println("Black, what is your move?");
-            }
-
-            //gets the move
-            String move = input.nextLine();
-
-            //call different class to check move validation
-            if (inCheck) {
-                //ensure that the check is working
-            }
-
-            //if valid move
-            //check if last move put king in check
-
-            whoseMove = !whoseMove;
-        }
     }
 
-    String checkNotation(String move) {
+    static String checkNotation(String move) {
         String returnString = "";
 
         Pattern piecePattern = Pattern.compile("[RNBQK]", Pattern.CASE_INSENSITIVE);
